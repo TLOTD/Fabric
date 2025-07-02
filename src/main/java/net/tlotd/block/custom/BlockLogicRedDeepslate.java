039ed540fb72f0387168e1190eb1c6b0c607c409
Isla_Nublar <87873220+Isla-Nublar@users.noreply.github.com>
2025-07-02 05:33:31 +0200

    Small Update

---
+++ src/main/java/net/tlotd/block/custom/BlockLogicRedDeepslate.java
@@ -0,0 +1,30 @@
+package net.tlotd.block.custom;
+
+import net.minecraft.core.block.Block;
+import net.minecraft.core.block.BlockLogic;
+import net.minecraft.core.block.entity.TileEntity;
+import net.minecraft.core.block.material.Material;
+import net.minecraft.core.enums.EnumDropCause;
+import net.minecraft.core.item.ItemStack;
+import net.minecraft.core.world.World;
+import net.tlotd.block.ModBlocks;
+
+public class BlockLogicRedDeepslate extends BlockLogic {
+	public BlockLogicRedDeepslate(Block block) {
+		super(block, Material.stone);
+	}
+
+	public ItemStack[] getBreakResult(World world, EnumDropCause dropCause, int x, int y, int z, int meta, TileEntity tileEntity) {
+		switch (dropCause) {
+			case SILK_TOUCH:
+			case PICK_BLOCK:
+				return new ItemStack[]{new ItemStack(this)};
+			case EXPLOSION:
+			case PROPER_TOOL:
+			case PISTON_CRUSH:
+				return new ItemStack[]{new ItemStack(ModBlocks.COBBLED_RED_DEEPSLATE)};
+			default:
+				return null;
+		}
+	}
+}
